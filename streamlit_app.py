90e6f1b8384a3838fdcad49de33f53b7a0b6828a
ronaldwalther <109809131+ronaldwalther@users.noreply.github.com>
2022-07-22 16:37:31 +0200

    Update streamlit_app.py
    und noch mal

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('The litte diner demo')
+
+streamlit.header('header text')
+
+streamlit.text('normal text1')
+
+streamlit.text('normal text2')
+streamlit.text('normal text3')
+streamlit.text('normal text4')
