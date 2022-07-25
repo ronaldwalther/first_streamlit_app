b8e583643c15d88abad9a14deaf8babfcf242601
ronaldwalther <109809131+ronaldwalther@users.noreply.github.com>
2022-07-25 13:36:39 +0200

    Update streamlit_app.py
    +pizze

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Ronnies healthy Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+streamlit.text('Pizza with 4 types of cheese')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
