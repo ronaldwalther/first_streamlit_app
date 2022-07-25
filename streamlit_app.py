2dc103339c54b4663985b968e92049b44d0bddb9
ronaldwalther <109809131+ronaldwalther@users.noreply.github.com>
2022-07-25 13:50:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 streamlit.text('🍕 Pizza with 4 types of cheese')
-
+streamlit.text('Breakfast Lasagne smotherd in cheese')
+               
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
