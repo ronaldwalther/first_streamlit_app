65cad91bee03f531da86216677a5cfab3ab0b338
ronaldwalther <109809131+ronaldwalther@users.noreply.github.com>
2022-07-29 17:53:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header("Fruityvice Fruit Advice!")
